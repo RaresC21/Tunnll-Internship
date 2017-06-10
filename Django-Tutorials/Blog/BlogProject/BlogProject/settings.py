73476343de1d2240d066a4f2404f8a929ebc28e4
Rares Cristian <raresc2105@gmail.com>
2017-06-09 23:38:26 -0400

    Restructure. Place blog folder within BlogProject

--- Django-Tutorials/Blog/BlogProject/BlogProject/settings.py
+++ Django-Tutorials/Blog/BlogProject/BlogProject/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'BlogProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'blogproject.db',
     }
 }
 
